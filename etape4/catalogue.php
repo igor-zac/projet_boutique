0b9f5db0cb20906bf55c30d6da6df49f7160b614
Igor <igor.zachcial@le-campus-numerique.fr>
2020-01-27 17:36:39 +0100

    création dossier etape 4

---
+++ etape4/catalogue.php
@@ -0,0 +1,20 @@
+<!doctype html>
+
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Titre de la page</title>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+        <header>
+            <h1>Nom de la boutique</h1>
+        </header>
+<!-- Appelle le fichier article.inc.php pour générer les blocs article de la page -->
+            <?php
+
+                include("article.inc.php");
+
+            ?>
+    </body>
+</html>
