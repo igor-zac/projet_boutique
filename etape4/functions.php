8cd9ea6eece8e22488ffcdaccb51235edb278e74
Igor <igor.zachcial@le-campus-numerique.fr>
2020-01-28 10:04:11 +0100

    commente le fichier functions.php

--- etape4/functions.php
+++ etape4/functions.php
@@ -1,16 +1,20 @@
 <?php
-
+// Tableau d'articles destiné à être appelé par la fonction "afficheArticle(...)
 $articles = [
     "Article 1" => ["Article 4", 300, "article4.jpg"],
     "Article 2" => ["Article 5", 50, "article5.jpg"],
     "Article 3" => ["Article 6", 500, "article6.jpg"]
 ];
 
+// Boucle itérant a travers le tableau précédent et appelant la fonction afficheArticle sur chaqueArticle
 foreach($articles as $article){
     afficheArticle($article[2], $article[0], $article[1]);
 }
 
-
+/*================================================================================================
+3 fonctions permettant d'afficher des articles, ne prenant aucun paramètre et sans valeur de retour
+structure semblable au code de l'étape 2
+================================================================================================*/
 function afficheArticle1(){
     $article = ["Article 1", 100, "article1.jpg"]; ?>
     <div class="article">
@@ -59,6 +63,10 @@ function afficheArticle3(){
 <?php
 }
 
+/*=====================================================================================================
+fonction prenant en paramètre le nom de l'article, le nom du fichier jpg et l'entier correspondant au prix
+la fonction se charge d'afficher les articles entrés au même format que les fonctions précédentes
+====================================================================================================*/
 function afficheArticle(String $nom_fichier, String $nom_article, int $prix){ ?>
     <div class="article">
         <div>
