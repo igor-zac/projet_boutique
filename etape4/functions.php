c8f1a62b4a6f7f9222468bcf2740c022abd44e8a
Igor <igor.zachcial@le-campus-numerique.fr>
2020-01-28 09:53:51 +0100

    ajoute une fonction d'affichage qui prend des parametres
    ajoute les images correspondantes aux nouveaux articles

--- etape4/functions.php
+++ etape4/functions.php
@@ -1,10 +1,14 @@
 <?php
 
 $articles = [
-    "Article 1" => ["Article 1", 100, "article1.jpg"],
-    "Article 2" => ["Article 2", 400, "article2.jpg"],
-    "Article 3" => ["Article 3", 200, "article3.jpg"]
+    "Article 1" => ["Article 4", 300, "article4.jpg"],
+    "Article 2" => ["Article 5", 50, "article5.jpg"],
+    "Article 3" => ["Article 6", 500, "article6.jpg"]
 ];
+
+foreach($articles as $article){
+    afficheArticle($article[2], $article[0], $article[1]);
+}
 
 
 function afficheArticle1(){
@@ -50,6 +54,21 @@ function afficheArticle3(){
 
         <div class="prix">
             <p><?php echo $article[1]?> €</p>
+        </div>
+    </div>
+<?php
+}
+
+function afficheArticle(String $nom_fichier, String $nom_article, int $prix){ ?>
+    <div class="article">
+        <div>
+            <img class="image_article" src=img/<?php echo $nom_fichier?> alt="L'image de mon article">
+        </div>
+
+        <h2><?php echo $nom_article?></h2>
+
+        <div class="prix">
+            <p><?php echo $prix?> €</p>
         </div>
     </div>
 <?php
