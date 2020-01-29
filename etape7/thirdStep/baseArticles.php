ee219368a2bd1db3158f98970118abf52e583db7
Igor <igor.zachcial@le-campus-numerique.fr>
2020-01-29 17:40:26 +0100

    externalise le tableau des articles dans un fichier baseArticles.php

---
+++ etape7/thirdStep/baseArticles.php
@@ -0,0 +1,44 @@
+<?php
+function getArr()
+{
+    $articles = [
+        "article_1" => [
+            "nom" => "Article 1",
+            "prix" => 300,
+            "img" => "article1.jpg",
+            "quantite" => 0
+        ],
+        "article_2" => [
+            "nom" =>  "Article 2",
+            "prix" => 400,
+            "img" => "article2.jpg",
+            "quantite" => 0
+        ],
+        "article_3" => [
+            "nom" => "Article 3",
+            "prix" => 150,
+            "img" => "article3.jpg",
+            "quantite" => 0
+        ],
+        "article_4" => [
+            "nom" => "Article 4",
+            "prix" => 300,
+            "img" => "article4.jpg",
+            "quantite" => 0
+        ],
+        "article_5" => [
+            "nom" => "Article 5",
+            "prix" => 50,
+            "img" => "article5.jpg",
+            "quantite" => 0
+        ],
+        "article_6" => [
+            "nom" => "Article 6",
+            "prix" => 500,
+            "img" => "article6.jpg",
+            "quantite" => 0
+        ]
+    ];
+
+    return $articles;
+}
