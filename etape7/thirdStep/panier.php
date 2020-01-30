6cd761e283fb004a4c6c7446dd6f89ba3d1c72bd
Igor <igor.zachcial@le-campus-numerique.fr>
2020-01-30 09:53:29 +0100

    mise en place du recalcul du panier en fonction des nouvelles quantités ainsi que la suppression des articles

--- etape7/thirdStep/panier.php
+++ etape7/thirdStep/panier.php
@@ -20,17 +20,26 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET'){
         }
 
     }  else {
-        $articles_choisis = unserialize($panier);
-        if(isset($_GET['recalc'])){
 
-        } else {
+        $articles_choisis = unserialize($_GET['panier']);
 
+        if(isset($_GET['delete'])){
+            unset($articles_choisis[$_GET['delete']]);
         }
+
+
+        foreach($articles_choisis as $key => $article){
+
+            $articles_choisis[$key]['quantite'] = $_GET[$key];
+        }
+
 
     }
 }
 
+
 $panier = serialize($articles_choisis);
+
 $total = totalPanier($articles_choisis);
 ?>
 
@@ -49,7 +58,7 @@ $total = totalPanier($articles_choisis);
         </header>
         <!-- Inclus le fichier functions.php et appelle ses fonctions pour générer les blocs article de la page -->
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="get" class="block">
-            <input type="hidden" name="panier" value="<?= $panier ?>">
+            <input type="hidden" name="panier" value='<?= $panier ?>'>
         <?php
         afficheArticles($articles_choisis, false);
         ?>
--- etape7/thirdStep/quantites.php
+++ etape7/thirdStep/quantites.php
@@ -5,7 +5,7 @@ function affichageQuantite(int $quantite, string $key)
     <div class="quantities d-flex flex-column">
         <div class="quantity d-flex flex-row">
             <label>Quantité :
-                <input type="number" value="<?= $quantite ?>">
+                <input type="number" name="<?= $key ?>" value="<?= $quantite ?>">
             </label>
         </div>
         <button type="submit" name="delete" value="<?= $key ?>" class="btn btn-secondary">Supprimer</button>
